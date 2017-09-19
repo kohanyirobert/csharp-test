21c72d20d86bd66a227c3b1e0013803c5100bc39
Kohányi Róbert <kohanyi.robert@gmail.com>
2017-09-19 12:03:42 +0200

    Initial commit
    Add indexer-test project.

--- indexer-test/Program.cs
+++ indexer-test/Program.cs
@@ -1,22 +1,12 @@
 ﻿using System;
-using System.Runtime.CompilerServices;
 
 namespace IndexerTest
 {
-    enum LotteryTicketNumberIndex
-    {
-        FIRST,
-        SECOND,
-        THIRD,
-        FOURTH,
-        FIFTH
-    }
-
     class LotteryTicket
     {
-        readonly int[] numbers = new int[5];
+        private int[] numbers = new int[5];
 
-        public LotteryTicket(int first, int second, int third, int fourth, int fifth)
+        internal LotteryTicket(int first, int second, int third, int fourth, int fifth)
         {
             numbers[0] = first;
             numbers[1] = second;
@@ -25,22 +15,19 @@ namespace IndexerTest
             numbers[4] = fifth;
         }
 
-        public int this[LotteryTicketNumberIndex i]
+        internal int this[int i]
         {
-            get { return numbers[(int)i]; }
+            get { return numbers[i - 1];  }
         }
     }
 
     class Program
     {
+
         static void Main(string[] args)
         {
-            LotteryTicket t = new LotteryTicket(12, 44, 54, 22, 23);
-            Console.WriteLine("The first number is {0}", t[LotteryTicketNumberIndex.FIRST]);
-            Console.WriteLine("The second number is {0}", t[LotteryTicketNumberIndex.SECOND]);
-            Console.WriteLine("The third number is {0}", t[LotteryTicketNumberIndex.THIRD]);
-            Console.WriteLine("The fourth number is {0}", t[LotteryTicketNumberIndex.FOURTH]);
-            Console.WriteLine("The fifth number is {0}", t[LotteryTicketNumberIndex.FIFTH]);
+            LotteryTicket ticket = new LotteryTicket(23, 34, 45, 56, 67);
+            Console.WriteLine("The third number is {0}", ticket[3]);
             Console.ReadKey();
         }
     }
