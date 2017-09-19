d2ff1b281c1448caf3055da74207ac920197a94c
Kohányi Róbert <kohanyi.robert@gmail.com>
2017-09-19 11:46:54 +0200

    Initial commit
    Add indexer-test code.

---
+++ indexer-test/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+
+namespace IndexerTest
+{
+    enum LotteryTicketNumberIndex
+    {
+        FIRST,
+        SECOND,
+        THIRD,
+        FOURTH,
+        FIFTH
+    }
+
+    class LotteryTicket
+    {
+        readonly int[] numbers = new int[5];
+
+        public LotteryTicket(int first, int second, int third, int fourth, int fifth)
+        {
+            numbers[0] = first;
+            numbers[1] = second;
+            numbers[2] = third;
+            numbers[3] = fourth;
+            numbers[4] = fifth;
+        }
+
+        public int this[LotteryTicketNumberIndex i]
+        {
+            get { return numbers[(int)i]; }
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            LotteryTicket t = new LotteryTicket(12, 44, 54, 22, 23);
+            Console.WriteLine("The first number is {0}", t[LotteryTicketNumberIndex.FIRST]);
+            Console.WriteLine("The second number is {0}", t[LotteryTicketNumberIndex.SECOND]);
+            Console.WriteLine("The third number is {0}", t[LotteryTicketNumberIndex.THIRD]);
+            Console.WriteLine("The fourth number is {0}", t[LotteryTicketNumberIndex.FOURTH]);
+            Console.WriteLine("The fifth number is {0}", t[LotteryTicketNumberIndex.FIFTH]);
+            Console.ReadKey();
+        }
+    }
+}
